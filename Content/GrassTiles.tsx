858d0bcbe87e0441f8cf86492fb315d75a016bac
CodeMyst <codemyst@outlook.com>
2018-02-14 19:39:24 +0100

    Init

---
+++ Content/GrassTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Grass" tilewidth="500" tileheight="500" tilecount="15" columns="3">
+ <image source="../../Game/endless-nameless/EndlessNameless.Unity/Assets/Graphics/Sprites/LevelSet1/GrassTiles.png" width="1500" height="2500"/>
+</tileset>
---
+++ Game1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+using TiledSharp;
+
+using MonoMyst.TileSystem;
+
+namespace MonoMyst
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private TmxMap map;
+        private Texture2D tileset;
+
+        private TileSystem.TileSystem tileSystem;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            map = new TmxMap ("Content/LevelSet1.tmx");
+            tileset = Content.Load<Texture2D> ("Grass");
+            tileSystem = new TileSystem.TileSystem (map, tileset);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin ();
+
+            tileSystem.Draw (spriteBatch);
+
+            spriteBatch.End ();
+
+            base.Draw(gameTime);
+        }
+    }
+}
