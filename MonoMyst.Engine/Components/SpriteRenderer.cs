61905a2ef4c0175dd6f597715de60ae34e634ccf
CodeMyst <codemyst@outlook.com>
2018-02-25 16:46:01 +0100

    SpriteRenderer component

---
+++ MonoMyst.Engine/Components/SpriteRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using MonoMyst.Core.ECS;
+
+namespace MonoMyst.Engine.Components
+{
+    public class SpriteRenderer : Component
+    {
+        public Texture2D Sprite;
+
+        public Vector2 Size;
+
+        public override void Draw (SpriteBatch spriteBatch)
+        {
+            if (Sprite == null)
+                return;
+
+            spriteBatch.Draw (Sprite, new Rectangle ((int) Entity.Position.X, (int) Entity.Position.Y, (int) Size.X, (int) Size.Y), null, Color.White);
+        }
+    }
+}
